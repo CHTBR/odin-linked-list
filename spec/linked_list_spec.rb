6c7ae89953f4656792319bcd1c23252d0300a98d
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-04 22:52:41 +0200

    Add public interface descriptions

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,4 +2,42 @@ require "rspec"
 require_relative "../lib/linked_list"
 
 RSpec.describe LinkedList do
+  describe "::new" do
+  end
+
+  describe "#append" do
+  end
+
+  describe "#prepend" do
+  end
+
+  describe "#size" do
+  end
+
+  describe "#head" do
+  end
+
+  describe "#tail" do
+  end
+
+  describe "#at" do
+  end
+
+  describe "#pop" do
+  end
+
+  describe "#contains?" do
+  end
+
+  describe "#find" do
+  end
+
+  describe "#to_s" do
+  end
+
+  describe "#insert_at" do
+  end
+
+  describe "#remove_at" do
+  end
 end
