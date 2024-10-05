c25e583fb3803a2ca3fc5d6afaccd7a2e0029cb1
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-05 11:47:28 +0200

    Add initialization method with optional array argument

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,3 +1,19 @@
+require_relative "node"
+
 # A class for managing a linked list DS and containing related methods
 class LinkedList
+  attr_accessor :head
+
+  def initialize(*args)
+    return unless args[0]
+
+    initial_args = args[0]
+    @head = Node.new(initial_args[0])
+    current_node = head
+    initial_args[1..].each do |element|
+      new_node = Node.new(element)
+      current_node.next_address = new_node
+      current_node = new_node
+    end
+  end
 end
