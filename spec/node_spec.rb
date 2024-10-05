e21d42e99b8999157e8715042d54eb1a7f8a815a
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-05 11:21:46 +0200

    Add test for data getter function

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,7 +2,12 @@ require "rspec"
 require_relative "../lib/node"
 
 RSpec.describe Node do
+  let(:subject) { Node.new("data") }
+
   describe "#data" do
+    it "returns data given on initialization" do
+      expect(subject.data).to eql("data")
+    end
   end
 
   describe "#next_address" do
