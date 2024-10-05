34b4fc2406a21c1961781daf0f4640e6b3602a35
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-05 08:58:13 +0200

    Add interface description

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,4 +2,12 @@ require "rspec"
 require_relative "../lib/node"
 
 RSpec.describe Node do
+  describe "#data" do
+  end
+
+  describe "#next_address" do
+  end
+
+  describe "#next_address=" do
+  end
 end
