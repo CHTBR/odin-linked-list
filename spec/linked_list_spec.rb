2b426ea139d20d65d963596099d23767f466f8a2
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-05 07:18:36 +0200

    Use shorter version for initializing LinkedList with values

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -40,20 +40,14 @@ RSpec.describe LinkedList do
   describe "#to_s" do
     context "when we append the values 1, 2, 3" do
       it "returns a string containing all the elements in the LinkedList" do
-        linked_list = LinkedList.new
-        linked_list.append(1)
-        linked_list.append(2)
-        linked_list.append(3)
+        linked_list = LinkedList.new([1, 2, 3])
         expect(linked_list.to_s).to eql("1 -> 2 -> 3 -> nil")
       end
     end
 
     context "when we append the values 2, 3, 1" do
       it "returns a string containing all the elements in the LinkedList" do
-        linked_list = LinkedList.new
-        linked_list.append(2)
-        linked_list.append(3)
-        linked_list.append(1)
+        linked_list = LinkedList.new([2, 3, 1])
         expect(linked_list.to_s).to eql("2 -> 3 -> 1 -> nil")
       end
     end
