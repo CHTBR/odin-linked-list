9997d828511cf734f26209b944305bc79b5ace76
Filip Popelka <117486280+CHTBR@users.noreply.github.com>
2024-10-05 12:02:33 +0200

    Rename head var to head_address to avoid conflict with head method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,14 +2,14 @@ require_relative "node"
 
 # A class for managing a linked list DS and containing related methods
 class LinkedList
-  attr_accessor :head
+  attr_accessor :head_address
 
   def initialize(*args)
     return unless args[0]
 
     initial_args = args[0]
-    @head = Node.new(initial_args[0])
-    current_node = head
+    @head_address = Node.new(initial_args[0])
+    current_node = head_address
     initial_args[1..].each do |element|
       new_node = Node.new(element)
       current_node.next_address = new_node
@@ -18,10 +18,10 @@ class LinkedList
   end
 
   def to_s
-    return nil if head.data.nil?
+    return nil if head_address.data.nil?
 
     data_arr = []
-    current_node = head
+    current_node = head_address
     until current_node.nil?
       data_arr << current_node.data
       current_node = current_node.next_address
@@ -31,7 +31,7 @@ class LinkedList
   end
 
   def tail
-    return "nil" if head.nil?
+    return "nil" if head_address.nil?
 
     last_node = _traverse
     last_node.data
@@ -40,7 +40,7 @@ class LinkedList
   private
 
   def _traverse
-    current_node = head
+    current_node = head_address
     current_node = current_node.next_address until current_node.next_address.nil?
     current_node
   end
